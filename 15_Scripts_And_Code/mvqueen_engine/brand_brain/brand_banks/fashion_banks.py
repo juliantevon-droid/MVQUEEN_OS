916fa0241cf4644bb20cf2f6f048453c752827f9
juliantevon-droid <juliantevon@gmail.com>
2026-08-29 09:08:38 -0400

    ADD: establish controlled fashion vocabulary bank

---
+++ 15_Scripts_And_Code/mvqueen_engine/brand_brain/brand_banks/fashion_banks.py
@@ -0,0 +1,9 @@
+"""Controlled fashion vocabulary for MVQueen editorial enrichment."""
+from __future__ import annotations
+
+PRODUCT_TERMS = ("dress", "top", "blouse", "shirt", "skirt", "trousers", "pants", "jeans", "jacket", "coat", "blazer", "cardigan", "sweater", "jumpsuit", "romper", "set", "bag", "shoe", "heel", "sandal", "boot", "sneaker", "scarf")
+SILHOUETTES = ("fitted", "relaxed", "oversized", "tailored", "slim", "straight", "wide-leg", "high-waisted", "cropped", "midi", "maxi", "mini")
+DETAILS = ("pleated", "ruched", "draped", "ribbed", "quilted", "button-front", "zip-front", "belted", "wrap", "asymmetric", "cutout", "pocketed")
+MATERIALS = ("cotton", "linen", "silk", "satin", "wool", "cashmere", "leather", "suede", "denim", "velvet", "knit", "jersey", "polyester", "nylon")
+
+__all__ = ["PRODUCT_TERMS", "SILHOUETTES", "DETAILS", "MATERIALS"]
