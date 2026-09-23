cb22bb29bfa3aa62d522a10cc9923b3c02c09d6f
juliantevon-droid <juliantevon@gmail.com>
2026-09-22 20:24:22 -0400

    Align exported Shopify API version with runtime configuration

--- app/shopify.server.ts
+++ app/shopify.server.ts
@@ -20,7 +20,7 @@ const shopify = shopifyApp({
 });
 
 export default shopify;
-export const apiVersion = ApiVersion.October25;
+export const apiVersion = ApiVersion.July26;
 export const addDocumentResponseHeaders = shopify.addDocumentResponseHeaders;
 export const authenticate = shopify.authenticate;
 export const unauthenticated = shopify.unauthenticated;
