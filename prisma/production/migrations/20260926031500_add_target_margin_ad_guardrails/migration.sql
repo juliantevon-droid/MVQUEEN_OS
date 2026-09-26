f0133b6b8e8277d85fd3fd723261de0023722b51
juliantevon-droid <juliantevon@gmail.com>
2026-09-26 02:51:09 -0400

    feat(commercial): add production target-margin ad guardrails

---
+++ prisma/production/migrations/20260926031500_add_target_margin_ad_guardrails/migration.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "ProductCommercialHealthState" ADD COLUMN "maxCacAtTargetMargin" DOUBLE PRECISION;
+ALTER TABLE "ProductCommercialHealthState" ADD COLUMN "targetMarginRoasFloor" DOUBLE PRECISION;
+ALTER TABLE "ProductCommercialHealthSnapshot" ADD COLUMN "maxCacAtTargetMargin" DOUBLE PRECISION;
+ALTER TABLE "ProductCommercialHealthSnapshot" ADD COLUMN "targetMarginRoasFloor" DOUBLE PRECISION;
