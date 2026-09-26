e23a8c4b184ecea7d849f561785eb625f259b58f
juliantevon-droid <juliantevon@gmail.com>
2026-09-26 02:51:01 -0400

    feat(commercial): migrate target-margin ad guardrails

---
+++ prisma/migrations/20260926031500_add_target_margin_ad_guardrails/migration.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "ProductCommercialHealthState" ADD COLUMN "maxCacAtTargetMargin" REAL;
+ALTER TABLE "ProductCommercialHealthState" ADD COLUMN "targetMarginRoasFloor" REAL;
+ALTER TABLE "ProductCommercialHealthSnapshot" ADD COLUMN "maxCacAtTargetMargin" REAL;
+ALTER TABLE "ProductCommercialHealthSnapshot" ADD COLUMN "targetMarginRoasFloor" REAL;
